3257cbc77c369df63609f0c04299257ddefcf1c1
cedric CASTRY <cedric.castry@gmail.com>
2025-03-12 10:19:38 +0000

    Sommes temps d'antenne

--- models/intermediate/intermediate_lepen_second_tour.sql
+++ models/intermediate/intermediate_lepen_second_tour.sql
@@ -1,15 +1,12 @@
---SELECt *
---from {{ ref('stg_projet_lewagon__antenne_lepen_premier_tour') }}
-
 WITH details_des_temps AS (
   SELECT 
     SAFE.PARSE_TIME('%H:%M:%S', CAST(Tranche__6h_9h__dur__e_ AS STRING)) AS Tranche_6h_9h,
     SAFE.PARSE_TIME('%H:%M:%S', CAST(Tranche__9h_18h__dur__e_ AS STRING)) AS Tranche_9h_18h,
     SAFE.PARSE_TIME('%H:%M:%S', CAST(Tranche__18h_24h__dur__e_ AS STRING)) AS Tranche_18h_24h,
     SAFE.PARSE_TIME('%H:%M:%S', CAST(Tranche__0h_6h__dur__e_ AS STRING)) AS Tranche_0h_6h
-  FROM `omega-cider-448409-a9.projet_lewagon.antenne_lepen_premier_tour`
+  FROM `omega-cider-448409-a9.projet_lewagon.antenne_lepen_second_tour`
   WHERE LOWER(TRIM(`D__tail_des_temps`)) = "total temps d\'antenne"
-)
+) 
 
 SELECT 
   FORMAT('%02d:%02d:%02d', 

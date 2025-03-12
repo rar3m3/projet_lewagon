847f8786bcec0df7148ba1f3f2e21901d42e952a
thomas marie <thomasmarie090@gmail.com>
2025-03-12 08:58:37 +0000

    Fin cleaning depense politique

--- models/staging/projet_lewagon/depense_politique/stg_projet_lewagon__ME_depense.sql
+++ models/staging/projet_lewagon/depense_politique/stg_projet_lewagon__ME_depense.sql
@@ -8,10 +8,10 @@ source as (
 
 renamed as (
     SELECT 
-    string_field_0,
+    string_field_0 AS compte,
     LOWER(REGEXP_REPLACE(NORMALIZE(string_field_1, NFD), r'\p{M}', '')) AS intitule_de_compte,
     IFNULL(SAFE_CAST(REPLACE(REPLACE(string_field_2, " ", ""), ",", ".") AS FLOAT64), 0) AS depense_payees_par_le_mandataire,
-    IFNULL(string_field_3,0) AS depenses_payees_par_la_formation_politique,
+    IFNULL(SAFE_CAST(string_field_3 AS INT64), 0) AS depenses_payees_par_la_formation_politique,
     IFNULL(SAFE_CAST(REPLACE(REPLACE(string_field_4, " ", ""), ",", ".") AS FLOAT64), 0) AS concours_en_nature,
     IFNULL(SAFE_CAST(REPLACE(REPLACE(string_field_5, " ", ""), ",", ".") AS FLOAT64), 0) AS totaux
 FROM source

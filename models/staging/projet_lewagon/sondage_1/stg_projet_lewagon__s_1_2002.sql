6a51e748da06545d13b61543023ad18382ea36ce
thomas marie <thomasmarie090@gmail.com>
2025-03-12 13:28:06 +0000

    clean sondage

--- models/staging/projet_lewagon/sondage_1/stg_projet_lewagon__s_1_2002.sql
+++ models/staging/projet_lewagon/sondage_1/stg_projet_lewagon__s_1_2002.sql
@@ -11,22 +11,101 @@ renamed as (
     select
         sondeur,
         `date`,
-        arlette_laguiller_lo,
-        daniel_gluckstein_poi,
-        olivier_besancenot_lcr,
-        robert_hue_pcf,
-        lionel_jospin_ps,
-        christiane_taubira_prg,
-        `noël_mamere_lv`,
-        `jean-pierre_chevenement_mdc`,
-        corinne_lepage_cap21,
-        `françois_bayrou_udf`,
-        christine_boutin_frs,
-        jacques_chirac_rpr,
-        alain_madelin_dl,
-        `jean_saint-josse_cpnt`,
-        `jean-marie_le_pen_fn`,
-        bruno_megret_mnr
+            SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(arlette_laguiller_lo) > 2 THEN SUBSTR(arlette_laguiller_lo, 1, LENGTH(arlette_laguiller_lo) - 2)
+            ELSE arlette_laguiller_lo
+        END, ',', '.') AS FLOAT64) AS arlette_laguiller_lo_float,
+    
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(daniel_gluckstein_poi) > 2 THEN SUBSTR(daniel_gluckstein_poi, 1, LENGTH(daniel_gluckstein_poi) - 2)
+            ELSE daniel_gluckstein_poi
+        END, ',', '.') AS FLOAT64) AS daniel_gluckstein_poi_float,
+    
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(olivier_besancenot_lcr) > 2 THEN SUBSTR(olivier_besancenot_lcr, 1, LENGTH(olivier_besancenot_lcr) - 2)
+            ELSE olivier_besancenot_lcr
+        END, ',', '.') AS FLOAT64) AS olivier_besancenot_lcr_float,
+    
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(robert_hue_pcf) > 2 THEN SUBSTR(robert_hue_pcf, 1, LENGTH(robert_hue_pcf) - 2)
+            ELSE robert_hue_pcf
+        END, ',', '.') AS FLOAT64) AS robert_hue_pcf_float,
+    
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(lionel_jospin_ps) > 2 THEN SUBSTR(lionel_jospin_ps, 1, LENGTH(lionel_jospin_ps) - 2)
+            ELSE lionel_jospin_ps
+        END, ',', '.') AS FLOAT64) AS lionel_jospin_ps_float,
+    
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(christiane_taubira_prg) > 2 THEN SUBSTR(christiane_taubira_prg, 1, LENGTH(christiane_taubira_prg) - 2)
+            ELSE christiane_taubira_prg
+        END, ',', '.') AS FLOAT64) AS christiane_taubira_prg_float,
+    
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(`noël_mamere_lv`) > 2 THEN SUBSTR(`noël_mamere_lv`, 1, LENGTH(`noël_mamere_lv`) - 2)
+            ELSE `noël_mamere_lv`
+        END, ',', '.') AS FLOAT64) AS noel_mamere_lv_float,
+    
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(`jean-pierre_chevenement_mdc`) > 2 THEN SUBSTR(`jean-pierre_chevenement_mdc`, 1, LENGTH(`jean-pierre_chevenement_mdc`) - 2)
+            ELSE `jean-pierre_chevenement_mdc`
+        END, ',', '.') AS FLOAT64) AS jean_pierre_chevenement_mdc_float,
+    
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(corinne_lepage_cap21) > 2 THEN SUBSTR(corinne_lepage_cap21, 1, LENGTH(corinne_lepage_cap21) - 2)
+            ELSE corinne_lepage_cap21
+        END, ',', '.') AS FLOAT64) AS corinne_lepage_cap21_float,
+    
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(`françois_bayrou_udf`) > 2 THEN SUBSTR(`françois_bayrou_udf`, 1, LENGTH(`françois_bayrou_udf`) - 2)
+            ELSE `françois_bayrou_udf`
+        END, ',', '.') AS FLOAT64) AS francois_bayrou_udf_float,
+    
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(christine_boutin_frs) > 2 THEN SUBSTR(christine_boutin_frs, 1, LENGTH(christine_boutin_frs) - 2)
+            ELSE christine_boutin_frs
+        END, ',', '.') AS FLOAT64) AS christine_boutin_frs_float,
+    
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(jacques_chirac_rpr) > 2 THEN SUBSTR(jacques_chirac_rpr, 1, LENGTH(jacques_chirac_rpr) - 2)
+            ELSE jacques_chirac_rpr
+        END, ',', '.') AS FLOAT64) AS jacques_chirac_rpr_float,
+    
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(alain_madelin_dl) > 2 THEN SUBSTR(alain_madelin_dl, 1, LENGTH(alain_madelin_dl) - 2)
+            ELSE alain_madelin_dl
+        END, ',', '.') AS FLOAT64) AS alain_madelin_dl_float,
+    
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(`jean_saint-josse_cpnt`) > 2 THEN SUBSTR(`jean_saint-josse_cpnt`, 1, LENGTH(`jean_saint-josse_cpnt`) - 2)
+            ELSE `jean_saint-josse_cpnt`
+        END, ',', '.') AS FLOAT64) AS jean_saint_josse_cpnt_float,
+    
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(`jean-marie_le_pen_fn`) > 2 THEN SUBSTR(`jean-marie_le_pen_fn`, 1, LENGTH(`jean-marie_le_pen_fn`) - 2)
+            ELSE `jean-marie_le_pen_fn`
+        END, ',', '.') AS FLOAT64) AS jean_marie_le_pen_fn_float,
+    
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(bruno_megret_mnr) > 2 THEN SUBSTR(bruno_megret_mnr, 1, LENGTH(bruno_megret_mnr) - 2)
+            ELSE bruno_megret_mnr
+        END, ',', '.') AS FLOAT64) AS bruno_megret_mnr_float
 
     from source
 
--- models/staging/projet_lewagon/sondage_1/stg_projet_lewagon__s_1_2022.sql
+++ models/staging/projet_lewagon/sondage_1/stg_projet_lewagon__s_1_2022.sql
@@ -9,24 +9,82 @@ source as (
 renamed as (
 
     select
-        `Index`,
-        Sondeur,
-        `Date`,
-        `Échantillon`,
-        Arthaud_LO,
-        Poutou_NPA,
-        Roussel_PCF,
-        Melenchon_LFI,
-        Hidalgo_PS,
-        `Jadot_EÉLV`,
-        Macron_LREM,
-        Pecresse_LR,
-        `Lassalle_RES`,
-        `Dupont-aignan_DLF`,
-        Le_pen_RN,
-        Zemmour_REC
+    Sondeur,
+    `Date`,
+    `Échantillon`,
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(Arthaud_LO) > 2 THEN SUBSTR(Arthaud_LO, 1, LENGTH(Arthaud_LO) - 2)
+            ELSE Arthaud_LO
+        END, ',', '.') AS FLOAT64) AS Arthaud_LO_float,
 
-    from source
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(Poutou_NPA) > 2 THEN SUBSTR(Poutou_NPA, 1, LENGTH(Poutou_NPA) - 2)
+            ELSE Poutou_NPA
+        END, ',', '.') AS FLOAT64) AS Poutou_NPA_float,
+
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(Roussel_PCF) > 2 THEN SUBSTR(Roussel_PCF, 1, LENGTH(Roussel_PCF) - 2)
+            ELSE Roussel_PCF
+        END, ',', '.') AS FLOAT64) AS Roussel_PCF_float,
+
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(Melenchon_LFI) > 2 THEN SUBSTR(Melenchon_LFI, 1, LENGTH(Melenchon_LFI) - 2)
+            ELSE Melenchon_LFI
+        END, ',', '.') AS FLOAT64) AS Melenchon_LFI_float,
+
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(Hidalgo_PS) > 2 THEN SUBSTR(Hidalgo_PS, 1, LENGTH(Hidalgo_PS) - 2)
+            ELSE Hidalgo_PS
+        END, ',', '.') AS FLOAT64) AS Hidalgo_PS_float,
+
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(`Jadot_EÉLV`) > 2 THEN SUBSTR(`Jadot_EÉLV`, 1, LENGTH(`Jadot_EÉLV`) - 2)
+            ELSE `Jadot_EÉLV`
+        END, ',', '.') AS FLOAT64) AS Jadot_EELV_float,
+
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(Macron_LREM) > 2 THEN SUBSTR(Macron_LREM, 1, LENGTH(Macron_LREM) - 2)
+            ELSE Macron_LREM
+        END, ',', '.') AS FLOAT64) AS Macron_LREM_float,
+
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(Pecresse_LR) > 2 THEN SUBSTR(Pecresse_LR, 1, LENGTH(Pecresse_LR) - 2)
+            ELSE Pecresse_LR
+        END, ',', '.') AS FLOAT64) AS Pecresse_LR_float,
+
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(`Lassalle_RES`) > 2 THEN SUBSTR(`Lassalle_RES`, 1, LENGTH(`Lassalle_RES`) - 2)
+            ELSE `Lassalle_RES`
+        END, ',', '.') AS FLOAT64) AS Lassalle_RES_float,
+
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(`Dupont-aignan_DLF`) > 2 THEN SUBSTR(`Dupont-aignan_DLF`, 1, LENGTH(`Dupont-aignan_DLF`) - 2)
+            ELSE `Dupont-aignan_DLF`
+        END, ',', '.') AS FLOAT64) AS Dupont_aignan_DLF_float,
+
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(Le_pen_RN) > 2 THEN SUBSTR(Le_pen_RN, 1, LENGTH(Le_pen_RN) - 2)
+            ELSE Le_pen_RN
+        END, ',', '.') AS FLOAT64) AS Le_pen_RN_float,
+
+    SAFE_CAST(REPLACE(
+        CASE 
+            WHEN LENGTH(Zemmour_REC) > 2 THEN SUBSTR(Zemmour_REC, 1, LENGTH(Zemmour_REC) - 2)
+            ELSE Zemmour_REC
+        END, ',', '.') AS FLOAT64) AS Zemmour_REC_float
+
+from source
 
 )
 

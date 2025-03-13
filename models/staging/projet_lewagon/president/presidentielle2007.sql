716ca292cdbc96bcb58d523a32ac7bf47111f1ed
Alexis Delaroff <alex.delaroff@gmail.com>
2025-03-13 13:23:48 +0000

    created presidentielle 20XX

---
+++ models/staging/projet_lewagon/president/presidentielle2007.sql
@@ -0,0 +1,71 @@
+{{ config(
+    materialized='table'
+) }}
+
+WITH source AS (
+  SELECT * FROM {{ source('projet_lewagon', 'pres2007') }}
+),
+renamed AS (
+  WITH aggregated_data AS (
+    SELECT
+      CONCAT('FR-', dep) AS code_departement,
+      SUM(inscrits) AS inscrits,
+      SUM(votants) AS votants,
+      SUM(exprimes) AS exprimes,
+      SUM(voixLAGUILLER) AS LAGUILLER,
+      SUM(voixBESANCENOT) AS BESANCENOT,
+      SUM(voixSCHIVARDI) AS SCHIVARDI,
+      SUM(voixBUFFET) AS BUFFET,
+      SUM(voixBOVE) AS BOVE,
+      SUM(voixVOYNET) AS VOYNET,
+      SUM(voixROYAL) AS ROYAL,
+      SUM(voixNIHOUS) AS NIHOUS,
+      SUM(voixBAYROU) AS BAYROU,
+      SUM(voixSARKOZY) AS SARKOZY,
+      SUM(voixVILLIERS) AS VILLIERS,
+      SUM(voixLEPEN) AS LEPEN,
+      SUM(voixT2ROYAL) AS T2ROYAL,
+      SUM(voixT2SARKOZY) AS T2SARKOZY
+    FROM 
+      source
+    GROUP BY dep
+  )
+  SELECT 
+    ad.*,
+    x.column_name AS max_voix_t1,
+    x2.column_name AS max_voix_t2
+  FROM 
+    aggregated_data ad,
+    UNNEST([ 
+      STRUCT('LAGUILLER' AS column_name, LAGUILLER AS value),
+      STRUCT('BESANCENOT' AS column_name, BESANCENOT AS value),
+      STRUCT('SCHIVARDI' AS column_name, SCHIVARDI AS value),
+      STRUCT('BUFFET' AS column_name, BUFFET AS value),
+      STRUCT('BOVE' AS column_name, BOVE AS value),
+      STRUCT('VOYNET' AS column_name, VOYNET AS value),
+      STRUCT('ROYAL' AS column_name, ROYAL AS value),
+      STRUCT('NIHOUS' AS column_name, NIHOUS AS value),
+      STRUCT('BAYROU' AS column_name, BAYROU AS value),
+      STRUCT('SARKOZY' AS column_name, SARKOZY AS value),
+      STRUCT('VILLIERS' AS column_name, VILLIERS AS value),
+      STRUCT('LEPEN' AS column_name, LEPEN AS value)
+    ]) x,
+    UNNEST([ 
+      STRUCT('T2ROYAL' AS column_name, T2ROYAL AS value),
+      STRUCT('T2SARKOZY' AS column_name, T2SARKOZY AS value)
+    ]) x2
+  WHERE 
+    x.value = GREATEST(
+      LAGUILLER, BESANCENOT, SCHIVARDI, 
+      BUFFET, BOVE, VOYNET, 
+      ROYAL, NIHOUS, BAYROU, 
+      SARKOZY, VILLIERS, LEPEN
+    )
+    AND 
+    x2.value = GREATEST(
+      T2ROYAL, 
+      T2SARKOZY
+    )
+)
+SELECT * 
+FROM renamed
---
+++ models/staging/projet_lewagon/president/presidentielle2017.sql
@@ -0,0 +1,69 @@
+{{ config(
+    materialized='table'
+) }}
+
+WITH source AS (
+  SELECT * FROM {{ source('projet_lewagon', 'pres2017') }}
+),
+renamed AS (
+  WITH aggregated_data AS (
+    SELECT
+      CONCAT('FR-', dep) AS code_departement,
+      SUM(inscrits) AS inscrits,
+      SUM(votants) AS votants,
+      SUM(exprimes) AS exprimes,
+      SUM(voixarthaud) AS arthaud,
+      SUM(voixpoutou) AS poutou,
+      SUM(voixmelenchon) AS melenchon,
+      SUM(voixhamon) AS hamon,
+      SUM(voixcheminade) AS cheminade,
+      SUM(voixlassalle) AS lassalle,
+      SUM(voixmacron) AS macron,
+      SUM(voixfillon) AS fillon,
+      SUM(voixasselineau) AS asselineau,
+      SUM(voixdupontaignan) AS dupontaignan,
+      SUM(voixmlepen) AS mlepen,
+      SUM(voixt2macron) AS t2macron,
+      SUM(voixt2mlepen) AS t2mlepen
+    FROM 
+      source
+    GROUP BY dep
+  )
+  SELECT 
+    ad.*,
+    x.column_name AS max_voix_t1,
+    x2.column_name AS max_voix_t2
+  FROM 
+    aggregated_data ad,
+    UNNEST([ 
+      STRUCT('arthaud' AS column_name, arthaud AS value),
+      STRUCT('poutou' AS column_name, poutou AS value),
+      STRUCT('melenchon' AS column_name, melenchon AS value),
+      STRUCT('hamon' AS column_name, hamon AS value),
+      STRUCT('cheminade' AS column_name, cheminade AS value),
+      STRUCT('lassalle' AS column_name, lassalle AS value),
+      STRUCT('macron' AS column_name, macron AS value),
+      STRUCT('fillon' AS column_name, fillon AS value),
+      STRUCT('asselineau' AS column_name, asselineau AS value),
+      STRUCT('dupontaignan' AS column_name, dupontaignan AS value),
+      STRUCT('mlepen' AS column_name, mlepen AS value)
+    ]) x,
+    UNNEST([ 
+      STRUCT('t2macron' AS column_name, t2macron AS value),
+      STRUCT('t2mlepen' AS column_name, t2mlepen AS value)
+    ]) x2
+  WHERE 
+    x.value = GREATEST(
+      arthaud, poutou, melenchon, 
+      hamon, cheminade, lassalle, 
+      macron, fillon, asselineau, 
+      dupontaignan, mlepen
+    )
+    AND 
+    x2.value = GREATEST(
+      t2macron, 
+      t2mlepen
+    )
+)
+SELECT * 
+FROM renamed

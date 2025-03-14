1eef60cb7253a4bc09e61080c2ea5603a44048d6
Alexis Delaroff <alex.delaroff@gmail.com>
2025-03-14 08:47:46 +0000

    evqew

--- models/staging/projet_lewagon/sondage_1_Top_Parti_Politique/stg_projet_lewagon__s_1_Top_Parti_Politique_2002.sql
+++ models/staging/projet_lewagon/sondage_1_Top_Parti_Politique/stg_projet_lewagon__s_1_Top_Parti_Politique_2002.sql
@@ -15,42 +15,13 @@ renamed as (
 select
     sondeur,
     `date`,
-    SAFE_CAST(REPLACE(
-        CASE 
-            WHEN LENGTH(lionel_jospin_ps) > 2 THEN SUBSTR(lionel_jospin_ps, 1, LENGTH(lionel_jospin_ps) - 2)
-            ELSE lionel_jospin_ps
-        END, ',', '.') AS FLOAT64) AS gauche_ps,
-
-    SAFE_CAST(REPLACE(
-        CASE 
-            WHEN LENGTH(`noël_mamere_lv`) > 2 THEN SUBSTR(`noël_mamere_lv`, 1, LENGTH(`noël_mamere_lv`) - 2)
-            ELSE `noël_mamere_lv`
-        END, ',', '.') AS FLOAT64) AS gauche_lv,
-
-    SAFE_CAST(REPLACE(
-        CASE 
-            WHEN LENGTH(`jean-pierre_chevenement_mdc`) > 2 THEN SUBSTR(`jean-pierre_chevenement_mdc`, 1, LENGTH(`jean-pierre_chevenement_mdc`) - 2)
-            ELSE `jean-pierre_chevenement_mdc`
-        END, ',', '.') AS FLOAT64) AS gauche_mdc,
-
-    SAFE_CAST(REPLACE(
-        CASE 
-            WHEN LENGTH(`françois_bayrou_udf`) > 2 THEN SUBSTR(`françois_bayrou_udf`, 1, LENGTH(`françois_bayrou_udf`) - 2)
-            ELSE `françois_bayrou_udf`
-        END, ',', '.') AS FLOAT64) AS centre_udf,
-
-    SAFE_CAST(REPLACE(
-        CASE 
-            WHEN LENGTH(jacques_chirac_rpr) > 2 THEN SUBSTR(jacques_chirac_rpr, 1, LENGTH(jacques_chirac_rpr) - 2)
-            ELSE jacques_chirac_rpr
-        END, ',', '.') AS FLOAT64) AS droite_rpr,
-
-    SAFE_CAST(REPLACE(
-        CASE 
-            WHEN LENGTH(`jean-marie_le_pen_fn`) > 2 THEN SUBSTR(`jean-marie_le_pen_fn`, 1, LENGTH(`jean-marie_le_pen_fn`) - 2)
-            ELSE `jean-marie_le_pen_fn`
-        END, ',', '.') AS FLOAT64) AS ext_droite_fn
-
+    lionel_jospin_ps AS gauche_ps,
+    noel_mamere_lv AS gauche_lv,
+    jean_pierre_chevenement_mdc AS gauche_mdc,
+    francois_bayrou_udf AS centre_udf,
+    jacques_chirac_rpr AS droite_rpr,
+    jean_marie_le_pen_fn AS ext_droite_fn
+    
 from source
 
 )

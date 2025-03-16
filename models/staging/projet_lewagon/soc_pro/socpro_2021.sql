b5aa706aaf5e6d5bdfef72de52a10e00abac2349
Alexis Delaroff <alex.delaroff@gmail.com>
2025-03-16 13:10:05 +0000

    added years to socpro

--- models/staging/projet_lewagon/soc_pro/socpro_2021.sql
+++ models/staging/projet_lewagon/soc_pro/socpro_2021.sql
@@ -2,41 +2,35 @@
     materialized='table'
 ) }}
 
-WITH source AS (
-    SELECT * FROM {{ ref('intermediate_soc_pro_2021') }}
-),
-
-renamed AS (
-    SELECT
+SELECT
+    CAST("2021-01-01" AS date) AS annee,
         `Region`,
         `Departement`,
         `Libelle_de_departement`,
-        (agriculteurs_actifs_ayant_un_emploi_rp2021 +
+        CAST(agriculteurs_actifs_ayant_un_emploi_rp2021 +
          artisans_commercants_chefs_d_entreprise_actifs_ayant_un_emploi_rp2021 +
          cadres_et_professions_intellectuelles_superieures_actifs_ayant_un_emploi_rp2021 +
          professions_intermediaires_actifs_ayant_un_emploi_rp2021 +
          employes_actifs_ayant_un_emploi_rp2021 +
-         ouvriers_actifs_ayant_un_emploi_rp2021) AS total_actifs,
-        (agriculteurs_chomeurs_rp2021 +
+         ouvriers_actifs_ayant_un_emploi_rp2021 AS INT64) AS total_actifs,
+        CAST(agriculteurs_chomeurs_rp2021 +
          artisans_commercants_chefs_d_entreprise_chomeurs_rp2021 +
          cadres_et_professions_intellectuelles_superieures_chomeurs_rp2021 +
          professions_intermediaires_chomeurs_rp2021 +
          employes_chomeurs_rp2021 +
-         ouvriers_chomeurs_rp2021) AS total_chomeurs,
-        (agriculteurs_chomeurs_rp2021 +
-         agriculteurs_actifs_ayant_un_emploi_rp2021) AS total_agriculteurs,
-        (artisans_commercants_chefs_d_entreprise_actifs_ayant_un_emploi_rp2021 +
-         artisans_commercants_chefs_d_entreprise_chomeurs_rp2021) AS total_artisants_commercants_chefs,
-        (cadres_et_professions_intellectuelles_superieures_actifs_ayant_un_emploi_rp2021 +
-         cadres_et_professions_intellectuelles_superieures_chomeurs_rp2021) AS total_cadres,
-        (professions_intermediaires_actifs_ayant_un_emploi_rp2021 +
-         professions_intermediaires_chomeurs_rp2021) AS total_intermediaires,
-        (employes_actifs_ayant_un_emploi_rp2021 +
-         employes_chomeurs_rp2021) AS total_employes,
-        (ouvriers_actifs_ayant_un_emploi_rp2021 +
-         ouvriers_chomeurs_rp2021) AS total_ouvriers
+         ouvriers_chomeurs_rp2021 AS INT64) AS total_chomeurs,
+        CAST(agriculteurs_chomeurs_rp2021 +
+         agriculteurs_actifs_ayant_un_emploi_rp2021 AS INT64) AS total_agriculteurs,
+        CAST(artisans_commercants_chefs_d_entreprise_actifs_ayant_un_emploi_rp2021 +
+         artisans_commercants_chefs_d_entreprise_chomeurs_rp2021 AS INT64) AS total_artisants_commercants_chefs,
+        CAST(cadres_et_professions_intellectuelles_superieures_actifs_ayant_un_emploi_rp2021 +
+         cadres_et_professions_intellectuelles_superieures_chomeurs_rp2021 AS INT64) AS total_cadres,
+        CAST(professions_intermediaires_actifs_ayant_un_emploi_rp2021 +
+         professions_intermediaires_chomeurs_rp2021 AS INT64) AS total_intermediaires,
+        CAST(employes_actifs_ayant_un_emploi_rp2021 +
+         employes_chomeurs_rp2021 AS INT64) AS total_employes,
+        CAST(ouvriers_actifs_ayant_un_emploi_rp2021 +
+         ouvriers_chomeurs_rp2021 AS INT64) AS total_ouvriers
 
-    FROM source
-)
+FROM {{ ref('intermediate_soc_pro_2021') }}
 
-SELECT * FROM renamed
